2a0a380a0433c686431bbba7c4622a4f4fc21c63
turtle-insect <turtle.insect@gmail.com>
2017-12-27 00:10:39 +0900

    first commit

---
+++ FF1/ILineAnalysis.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FF1
+{
+	interface ILineAnalysis
+    {
+		bool Line(String[] oneLine); 
+    }
+}
---
+++ FF1/Util.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FF1
+{
+    class Util
+    {
+		public const uint BaseAddress = 0;
+		public const uint BlockSize = 0xECC;
+	}
+}
