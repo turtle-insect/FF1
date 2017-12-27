2643f4309e72f86b5e1115104df052e792e2f99d
turtle-insect <turtle.insect@gmail.com>
2017-12-28 00:03:38 +0900

    Add Charactor Param

---
+++ FF1/Charactor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FF1
+{
+	class Charactor
+	{
+		private readonly uint mAddress;
+		public Charactor(uint address)
+		{
+			mAddress = address;
+		}
+
+		public uint Job
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 16, 1);
+			}
+
+			set
+			{
+				SaveData.Instance().WriteNumber(mAddress + 16, 1, value);
+			}
+		}
+	}
+}
--- FF1/DataContext.cs
+++ FF1/DataContext.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace FF1
 {
     class DataContext
     {
-    }
+		public ObservableCollection<Charactor> Party { get; set; } = new ObservableCollection<Charactor>();
+
+		public DataContext()
+		{
+			for (uint i = 0; i < 4; i++)
+			{
+				Party.Add(new Charactor(0x1A + 92 * i));
+			}
+		}
+	}
 }
