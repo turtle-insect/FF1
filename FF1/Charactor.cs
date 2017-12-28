82ba02bef0a1b9d98389bcbb10be2545d3006a7e
turtle-insect <turtle.insect@gmail.com>
2017-12-29 00:50:22 +0900

    Add Charactor Param

--- FF1/Charactor.cs
+++ FF1/Charactor.cs
@@ -26,5 +26,161 @@ namespace FF1
 				SaveData.Instance().WriteNumber(mAddress + 16, 1, value);
 			}
 		}
+
+		public uint Lv
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 18, 1);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 18, 1, value, 1, 99);
+			}
+		}
+
+		public uint Exp
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 22, 4);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 22, 4, value, 0, 9999999);
+			}
+		}
+
+		public uint HP
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 26, 2);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 26, 2, value, 0, 999);
+			}
+		}
+
+		public uint MaxHP
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 28, 2);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 28, 2, value, 0, 999);
+			}
+		}
+
+		public uint MP
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 30, 2);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 30, 2, value, 0, 999);
+			}
+		}
+
+		public uint MaxMP
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 32, 2);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 32, 2, value, 0, 999);
+			}
+		}
+
+		public uint MagicLv
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 34, 1);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 34, 1, value, 0, 8);
+			}
+		}
+
+		public uint Power
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 35, 1);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 35, 1, value, 0, 255);
+			}
+		}
+
+		public uint Speed
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 36, 1);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 36, 1, value, 0, 255);
+			}
+		}
+
+		public uint Intelligence
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 37, 1);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 37, 1, value, 0, 255);
+			}
+		}
+
+		public uint Strength
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 38, 1);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 38, 1, value, 0, 255);
+			}
+		}
+
+		public uint Luck
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(mAddress + 39, 1);
+			}
+
+			set
+			{
+				Util.WriteNumber(mAddress + 39, 1, value, 0, 255);
+			}
+		}
 	}
 }
--- FF1/Util.cs
+++ FF1/Util.cs
@@ -10,5 +10,12 @@ namespace FF1
     {
 		public const uint BaseAddress = 0;
 		public const uint BlockSize = 0xECC;
+
+		public static void WriteNumber(uint address, uint size, uint value, uint min, uint max)
+		{
+			if (value < min) value = min;
+			if (value > max) value = max;
+			SaveData.Instance().WriteNumber(address, size, value);
+		}
 	}
 }
