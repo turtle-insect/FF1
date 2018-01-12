083a02f950d780f062e20dbe266d293dbfe65e59
turtle-insect <turtle.insect@gmail.com>
2018-01-13 02:34:40 +0900

    Refactor

--- FF1/DataContext.cs
+++ FF1/DataContext.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 
 namespace FF1
 {
-    class DataContext
+	class DataContext
     {
 		public ObservableCollection<Charactor> Party { get; set; } = new ObservableCollection<Charactor>();
 		public ObservableCollection<Item> Items { get; set; } = new ObservableCollection<Item>();
