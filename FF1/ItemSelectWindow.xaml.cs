7cc0f6e9b806a2220b8dc1822322dff485706941
turtle-insect <turtle.insect@gmail.com>
2018-01-16 00:21:20 +0900

    Refactor

--- FF1/ItemSelectWindow.xaml.cs
+++ FF1/ItemSelectWindow.xaml.cs
@@ -1,23 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace FF1
 {
-    /// <summary>
-    /// ItemSelectWindow.xaml の相互作用ロジック
-    /// </summary>
-    public partial class ItemSelectWindow : Window
+	/// <summary>
+	/// ItemSelectWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class ItemSelectWindow : Window
     {
 		public enum eKind
 		{
