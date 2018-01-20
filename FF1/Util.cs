2238b013d3dae6bbce0911fe3c5749626af27980
turtle-insect <turtle.insect@gmail.com>
2018-01-21 04:14:09 +0900

    Refactor

--- FF1/Util.cs
+++ FF1/Util.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FF1
+﻿namespace FF1
 {
-    class Util
+	class Util
     {
 		public const uint BaseAddress = 0;
 		public const uint BlockSize = 0xECC;
